9cdef6cce0b61850c0d7b394eece9089382c0ca2
Benji <67171031+liangbinjie@users.noreply.github.com>
2025-04-11 07:13:21 +0000

    project structure based on MERN STACK

---
+++ Schoolify/frontend/src/App.jsx
@@ -0,0 +1,29 @@
+import { Routes, Route } from "react-router-dom";
+import './App.css'
+import Navbar from './components/Navbar.jsx'
+import Home from './pages/Home.jsx'
+import LoginPage from './pages/Login.jsx'
+
+
+/* 
+
+Se define todo lo relacionado a rutas,
+Se define el "navbar" como un componente global en toda la app
+--> Agregar en un futuro rutas autorizadas y no autorizadas (es decir, si el usuario no esta logueado no podra ingresar a ciertas rutas)
+
+*/
+
+function App() {
+  return (
+    <>
+      <Navbar/>
+        <Routes>
+          <Route path="/" element={<Home/>} />
+          <Route path="/home" element={<Home/>} />
+          <Route path="/login" element={<LoginPage/>} />
+        </Routes>
+    </>
+  )
+}
+
+export default App
