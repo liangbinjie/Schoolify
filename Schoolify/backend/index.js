712ac4f3062a2586357228a9a02ab7ebc01cd41c
Mariano Mayorga <mamayorga@estudiantec.cr>
2025-04-13 10:19:46 -0600

    Update index.js

--- Schoolify/backend/index.js
+++ Schoolify/backend/index.js
@@ -14,6 +14,7 @@ app.use(cors());
 app.use(express.json());
 app.use("/user", userRouter);
 app.use("/course", courseRouter);
+app.use("/enrollment", enrollmentRouter);
 
 app.get("/", (req, res) => {
     return res.status(234).send("hello world");
